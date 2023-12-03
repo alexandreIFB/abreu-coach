791691a3512bf5082dd4f9edb1ce63d7f6371ae2
Alexandre Abreu <abreuxandi2010@gmail.com>
2023-12-03 13:18:40 -0300

    button new client

--- app/(tabs)/clients/index.tsx
+++ app/(tabs)/clients/index.tsx
@@ -1,10 +1,15 @@
 import { ClientsList } from '../../../components/ClientsList';
 import { Container } from './styles';
+import { AddIconFixed } from '../../../components/AddIconFixed';
+import {  TouchableOpacity } from 'react-native';
 
 export default function TabClientsScreen() {
   return (
     <Container>
       <ClientsList />
+      <TouchableOpacity>
+        <AddIconFixed />
+      </TouchableOpacity>
     </Container>
   );
 }
